55f37f98d497b1c6823ca14c35d555376807be90
inpeters <75335175+inpeters@users.noreply.github.com>
2022-10-24 23:35:43 -0400

    Add files via upload

---
+++ Planner/src/main/java/com/StudentPlanner/PlannerController.java
@@ -0,0 +1,25 @@
+package com.StudentPlanner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PlannerController {
+
+	@RequestMapping("/home")
+	public String home() 
+	{
+		return "home";
+	}
+	@RequestMapping("/calendar")
+	public String calendar() 
+	{
+		return "calendar";
+	}
+	@RequestMapping("/prioritypage")
+	public String prioritypage() 
+	{
+		return "prioritypage";
+	}
+	
+}
