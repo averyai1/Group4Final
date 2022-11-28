ba4fc57b4bb25c07161e05c1e1a9c46ab9de1045
inpeters <75335175+inpeters@users.noreply.github.com>
2022-11-28 16:59:58 -0500

    worked on planner.java and plannerserviceimpl.java

--- Planner/src/main/java/com/StudentPlanner/entitiy/Planner.java
+++ Planner/src/main/java/com/StudentPlanner/entitiy/Planner.java
@@ -1,5 +1,97 @@
 package com.StudentPlanner.entitiy;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="list")
 public class Planner {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	@Column(name="id")
+	private int id; 
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="date")
+	private int date; 
+	
+	@Column(name="priority")
+	private String priority;
+	
+	
+	public Planner() {
+		
+	}
+	
+	public Planner(int id, String name, String description, int date, String priority) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.date = date;
+		this.priority = priority;
+	}
+	
+	// set the getters and setters
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getDate() {
+		return date;
+	}
+	
+	public void setDate(int date) {
+		this.date = date;
+	}
+	
+	public String getPriority() {
+		return priority;
+	}
+	
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	
+	@Override
+	public String toString() {
+		return "Planner [id=" + id + ", name=" + name + ", description=" + description + ", date=" + date 
+				+ ", priority=" + priority + ", getId()=" + getId() + ", getName()" + getName() + ", getDescription()=" + getDescription() 
+				+ ", getDate()=" + getDate() + ", getPriority()=" + getPriority() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";	
+				
+				
+	}
 
 }
--- Planner/src/main/java/com/StudentPlanner/service/PlannerServiceImpl.java
+++ Planner/src/main/java/com/StudentPlanner/service/PlannerServiceImpl.java
@@ -1,5 +1,47 @@
 package com.StudentPlanner.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.StudentPlanner.dao.PlannerRepository;
+import com.StudentPlanner.entitiy.Planner;
+
 public class PlannerServiceImpl {
 
+	private PlannerRepository plannerRepository;
+	
+	@Autowired
+	public PlannerServiceImpl (PlannerRepository thePlannerRepository) {
+		plannerRepository = thePlannerRepository;
+		
+	}
+	public List<Planner> findAll() {
+		return plannerRepository.findAllByOrderByPriority();
+	}
+	
+	public Planner findById(int theId) {
+		Optional<Planner> plannerid = plannerRepository.findById(theId);
+		
+		Planner thePlanner = null;
+		
+		if (plannerid.isPresent()) {
+			thePlanner = plannerid.get();
+		}
+		else 
+		{
+			throw new RuntimeException("The PlannerId you've entered is invalid - " + theId);
+		}
+		return thePlanner;
+	}
+	
+	public void save(Planner thePlanner) {
+		plannerRepository.save(thePlanner);
+	}
+	
+	public void deletebyId(int theId) {
+		plannerRepository.deleteById(theId);
+	}
+	
 }
